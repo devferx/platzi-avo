f5b2a78a5d1e59ba73100ac62c3868a8b06b37a7
devferx <ferkki@hotmail.com>
2020-11-08 11:07:28 -0400

    mejora en el cors

--- pages/api/yes-or-no.ts
+++ pages/api/yes-or-no.ts
@@ -1,13 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiResponse, NextApiRequest } from 'next'
 import enablePublicAccess from '@cors'
 
-const yesOrNo = async (req: NextApiRequest, res: NextApiResponse) => {
+const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Generally, you would not want this in your apps.
+  // See more in 'cors.js'
   await enablePublicAccess(req, res)
+
   const answer = Math.round(Math.random()) ? 'yes' : 'no'
+
   res.status(200).json({
     data: answer,
     error: null,
   })
 }
 
-export default yesOrNo
+export default allAvos
