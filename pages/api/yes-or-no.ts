015ea2aea4bb78082f734321fa00ef490c41bbbe
devferx <ferkki@hotmail.com>
2020-11-08 10:56:23 -0400

    fixed yes or no

--- pages/api/yes-or-no.ts
+++ pages/api/yes-or-no.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const yesOrNo = async (req: NextApiRequest, res: NextApiResponse) => {
-
-
   const answer = Math.round(Math.random()) ? 'yes' : 'no'
   res.status(200).json({
     data: answer,
