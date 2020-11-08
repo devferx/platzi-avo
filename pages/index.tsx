74b445b920eedd9aa6e2453aadd654e491b601fb
devferx <ferkki@hotmail.com>
2020-11-08 14:14:09 -0400

    static generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Header from '@components/Header/Header'
 import CardList from '@components/CardList/CardList'
 import fetch from 'isomorphic-unfetch'
 
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const response = await fetch('https://platzi-avocados.vercel.app/api/avo')
   const { data: productList }: TAPIAvoResponse = await response.json()
 
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,30 +1,44 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
 import fetch from 'isomorphic-unfetch'
 import ProductHeader from '@components/ProductHeader/ProductHeader'
 import ProductInfo from '@components/ProductInfo/ProductInfo'
 import Loading from '@components/Loading/Loading'
 import styles from './product.module.css'
 
-const ProductPage = () => {
-  const [product, setProduct] = useState<TProduct>()
+export const getStaticPaths = async () => {
+  const response = await fetch('https://platzi-avocados.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
-  const {
-    query: { id },
-  } = useRouter()
+  const paths = productList.map(({ id }) => ({
+    params: {
+      id,
+    },
+  }))
 
-  useEffect(() => {
-    async function fetchData() {
-      if (id) {
-        const res = await fetch(`/api/avo/${id}`)
-        const data = await res.json()
-        setProduct(data)
-      }
-    }
+  return {
+    paths,
+    // Incremental static generation
+    // 404 for everything else
+    fallback: false,
+  }
+}
 
-    fetchData()
-  }, [id])
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string
+  const response = await fetch(
+    `https://platzi-avocados.vercel.app//api/avo/${id}`
+  )
+  const product: TProduct = await response.json()
 
+  return {
+    props: {
+      product,
+    },
+  }
+}
+
+const ProductPage = ({ product }: { product: TProduct }) => {
   return product?.name ? (
     <section className={styles.mainContainer}>
       <ProductHeader product={product} />
